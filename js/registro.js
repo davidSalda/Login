ba55165febb24fc1a10de1713ea0f955801de6be
David s <dadiortiz403@gmail.com>
2020-06-17 16:00:25 -0500

    guardando usuario

--- js/registro.js
+++ js/registro.js
@@ -20,5 +20,19 @@ function registro(e) {
         return;
     }
 
-    console.log('Validando datos');
+    const usuario ={
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal,
+        pass: passVal
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    nombre.value = '';
+    email.value = '';
+    user.value = '';
+    pass.value = '';
+
+    console.log('Usuario guardado');
 }
